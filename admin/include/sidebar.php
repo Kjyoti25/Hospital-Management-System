823d5cc1b2cd670e1d7cb9c350fdc2b380b6e12b
Kjyoti25 <jyotidhanarua@gmail.com>
2026-02-22 01:52:54 +0530

    1st time commit
    first

---
+++ admin/include/sidebar.php
@@ -0,0 +1,115 @@
+<div class="sidebar app-aside" id="sidebar">
+				<div class="sidebar-container perfect-scrollbar">
+
+<nav>
+
+						<!-- start: MAIN NAVIGATION MENU -->
+						<div class="navbar-title">
+							<span>Main Navigation</span>
+						</div>
+						<ul class="main-navigation-menu">
+							<li>
+								<a href="adddoctor.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-home"></i>
+										</div>
+										<div class="item-inner">
+
+											<span class="title">Add Doctor</span>
+										</div>
+									</div>
+								</a>
+								   <!-- <ul class="dropdown-menu">
+                                      <li><a class="dropdown-item" href="adddoctor.php"> Add</a></li>
+                                      <li><a class="dropdown-item" href="doctorview.php">  View</a></li>
+                                  </ul> -->
+							</li>
+							<li>
+								<a href="doctorview.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-home"></i>
+										</div>
+										<div class="item-inner">
+
+											<span class="title">View Doctor</span>
+										</div>
+									</div>
+								</a>
+								   
+							</li>
+							<li>
+								<a href="reception_add.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-home"></i>
+										</div>
+										<div class="item-inner">
+
+											<span class="title"> Add Reception</span>
+										</div>
+									</div>
+								</a>
+								<!-- <ul class="dropdown-menu">
+                                      <li><a class="dropdown-item" href="reception_add.php"> Add</a></li>
+                                      <li><a class="dropdown-item" href="reception_view.php">  View</a></li>
+                                  </ul> -->
+
+							</li>
+							<li>
+									<a href="reception_view.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-home"></i>
+										</div>
+										<div class="item-inner">
+
+											<span class="title"> View Reception</span>
+										</div>
+									</div>
+								</a>
+</li>	
+							
+
+
+<li>
+								<a href="view_patient_register.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-file"></i>
+										</div>
+										<div class="item-inner">
+
+											<span class="title">View Pasient  Register </span>
+										</div>
+									</div>
+								</a>
+							</li>
+
+
+<li>
+								<a href="view_all_appointment.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-file"></i>
+										</div>
+										<div class="item-inner">
+
+											<span class="title">View All Appointment </span>
+										</div>
+									</div>
+								</a>
+							</li>
+
+
+
+
+								</li>
+
+						</ul>
+						<!-- end: CORE FEATURES -->
+
+					</nav>
+					</div>
+			</div>
